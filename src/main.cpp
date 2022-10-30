bc9377d4286aa645257a1129412fa685690fba14
alortius <anna-alina.bondarets@ucu.edu.ua>
2022-10-30 22:40:12 +0200

    brrr

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,7 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
 #include <iostream>
-#include "options_parser.h"
 
 int main(int argc, char* argv[]) {
-    command_line_options_t command_line_options{argc, argv};
-    std::cout << "A flag value: " << command_line_options.get_A_flag() << std::endl;
     return 0;
 }
